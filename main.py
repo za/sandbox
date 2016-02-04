42f663dc0f9f9cfd673c41c0986dd030f402f4cb
za <za@python.or.id>
2016-02-04 14:16:32 +1100

    testing flake8

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ import sys
 def get_environment_variable(key):
     try:
         variable = os.environ[key]
-    except
+    except:
         pass
 
     return variable
 
+
 def get_environment_variable_token():
     try:
         variable = os.environ["token"]
+    except:
+        pass
+
+    return variable
+
+
+def get_environment_variable_mysql_passwd():
+    try:
+        variable = os.environ["mysql_passwd"]
     except
         pass
 
